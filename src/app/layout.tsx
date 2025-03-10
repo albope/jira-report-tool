b0285902e3a059d85532433ee09e1762a7e5482e
Alberto Bort <albertobort@gmail.com>
2025-03-10 13:03:51 +0100

    mejoras esteticas 12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Generador de Reportes JIRA",
+  title: "[EID] Generador Reportes JIRA",
   description: "Ejemplo minimalista con Next.js, Tailwind y TypeScript",
 };
 
