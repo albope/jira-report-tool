e4534d2d0850500e74a7b9ef89566e6716ffc401
Alberto Bort <albertobort@gmail.com>
2025-04-10 16:31:38 +0200

    Correción deploy Vercel

--- src/components/StepTwoForm.tsx
+++ src/components/StepTwoForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef} from "react";
 import { ParsedData } from "@/utils/parseJiraContent";
 import * as XLSX from "xlsx";
 import Tippy from "@tippyjs/react";
